e4198007a44f073516138f48b05df301ff22518f
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2022-06-21 10:08:52 +0200

    fixed second task

--- homework-1/script.js
+++ homework-1/script.js
@@ -16,17 +16,16 @@ if (
   console.log('Некорректный ввод');
 }
 
-const firstNumber = parseInt(prompt(`${TEXT_MESSAGE}1`), 10);
-const secondNumber = parseInt(prompt(`${TEXT_MESSAGE}2`), 10);
-if (
-  !Number.isNaN(firstNumber) &&
-  Number.isInteger(firstNumber) &&
-  !Number.isNaN(secondNumber) &&
-  Number.isInteger(secondNumber)
-) {
-  console.log(
-    `Ответ: ${firstNumber + secondNumber}, ${firstNumber / secondNumber}`
-  );
+const firstNumber = parseInt(prompt('Введите первое число'), 10);
+if (!Number.isNaN(firstNumber) && Number.isInteger(firstNumber)) {
+  const secondNumber = parseInt(prompt('Введите второе число'), 10);
+  if (!Number.isNaN(secondNumber) && Number.isInteger(secondNumber)) {
+    console.log(
+      `Ответ: ${firstNumber + secondNumber}, ${firstNumber / secondNumber}`
+    );
+  } else {
+    console.log('Некорректный ввод');
+  }
 } else {
   console.log('Некорректный ввод');
 }
